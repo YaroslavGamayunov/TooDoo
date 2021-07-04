a8e4b40924d4d72f999a0a5c7fd90f7774471f96
YaroslavGamayunov <yaroslav.gamayunov@gmail.com>
2021-07-04 14:58:50 +0300

    Fix bug in room database (schedule mode converter wasn't added)

--- app/src/main/java/com/yaroslavgamayunov/toodoo/data/db/TaskDatabase.kt
+++ app/src/main/java/com/yaroslavgamayunov/toodoo/data/db/TaskDatabase.kt
@@ -7,7 +7,7 @@ import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
 
 @Database(entities = [TaskEntity::class], version = 1, exportSchema = false)
-@TypeConverters(TimeConverter::class, PriorityConverter::class)
+@TypeConverters(TimeConverter::class, PriorityConverter::class, ScheduleModeConverter::class)
 abstract class TaskDatabase : RoomDatabase() {
     abstract fun taskDao(): TaskDao
 
@@ -17,6 +17,7 @@ abstract class TaskDatabase : RoomDatabase() {
             return Room.databaseBuilder(context, TaskDatabase::class.java, name)
                 .addTypeConverter(TimeConverter())
                 .addTypeConverter(PriorityConverter())
+                .addTypeConverter(ScheduleModeConverter())
                 .fallbackToDestructiveMigration().build()
         }
     }
