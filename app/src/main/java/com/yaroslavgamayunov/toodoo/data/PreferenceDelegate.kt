241e890707baa06a976d408b7539c20d514d9682
YaroslavGamayunov <yaroslav.gamayunov@gmail.com>
2021-07-16 16:16:38 +0300

    Add delegate for shared preferences

---
+++ app/src/main/java/com/yaroslavgamayunov/toodoo/data/PreferenceDelegate.kt
@@ -0,0 +1,42 @@
+package com.yaroslavgamayunov.toodoo.data
+
+import android.content.Context
+import android.content.SharedPreferences
+import androidx.core.content.edit
+import kotlin.properties.ReadWriteProperty
+import kotlin.reflect.KProperty
+
+class PreferenceDelegate<T>(
+    val context: Context,
+    private val preferenceKey: String,
+    private val defaultValue: T
+) : ReadWriteProperty<Any?, T> {
+
+    private val preferences: SharedPreferences by lazy {
+        context.getSharedPreferences(SHARED_PREFERENCES_NAME, Context.MODE_PRIVATE)
+    }
+
+    @Suppress("UNCHECKED_CAST")
+    override fun getValue(thisRef: Any?, property: KProperty<*>): T {
+        return when (defaultValue) {
+            is Long -> preferences.getLong(preferenceKey, defaultValue)
+            else -> null
+        } as? T ?: throw TypeNotSupportedByPreferencesDelegateException(defaultValue)
+    }
+
+    override fun setValue(thisRef: Any?, property: KProperty<*>, value: T) {
+        preferences.edit {
+            when (value) {
+                is Long -> putLong(preferenceKey, value)
+                else -> throw TypeNotSupportedByPreferencesDelegateException(defaultValue)
+            }
+        }
+    }
+
+    private companion object {
+        const val SHARED_PREFERENCES_NAME = "application_preferences"
+    }
+
+    class TypeNotSupportedByPreferencesDelegateException(value: Any?) :
+        RuntimeException("Preferences delegate does not have support for $value")
+}
